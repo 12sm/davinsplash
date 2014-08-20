86f526beed70c934129bf37a3e6010f86af54f8b
Ian Noyes <ian@12southmusic.com>
2014-08-20 15:15:07 -0500

    img path

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,5 +1,5 @@
 <div class='row'>
 	<div class='col-sm-12'>
-		<img src='/wp-content/themes/splash/logo.png' class="img-responsive logo">
+		<img src='/wp-content/themes/splash/assets/img/logo.png' class="img-responsive logo">
 	</div>
 </div>
